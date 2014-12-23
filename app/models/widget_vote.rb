d0ef069e1ea81fed876bea91039ac2a6d5ea96b0
Louise Crow <louise.crow@gmail.com>
2014-12-23 15:36:11 +0000

    Add some specs for widget vote validation.

--- app/models/widget_vote.rb
+++ app/models/widget_vote.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: widget_votes
+#
+#  id              :integer          not null, primary key
+#  cookie          :string(255)
+#  info_request_id :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class WidgetVote < ActiveRecord::Base
   belongs_to :info_request
   validates :info_request, :presence => true
